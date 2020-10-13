6ebae3435e18d93d8bd804b38a4b064ef90bbe81
Julius <jubeki99@gmail.com>
2020-10-13 18:25:34 +0200

    Add CountyRule

---
+++ src/Rules/CountyRule.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace Squire\Rules;
+
+use Illuminate\Contracts\Validation\Rule;
+use Squire\Models\Counties\GbCounty;
+use Squire\Models\County;
+
+class CountyRule implements Rule
+{
+    const COUNTIES = [
+        'gb' => GbCounty::class,
+    ];
+
+    protected $county;
+
+    public function __construct($county)
+    {
+        if (!array_key_exists($county, COUNTIES)) {
+            throw new \IllegalArgumentException("Unknown county code '$county'");
+        }
+        $this->county = $county;
+    }
+
+    /**
+     * Determine if the validation rule passes.
+     *
+     * @param  string  $attribute
+     * @param  mixed  $value
+     * @return bool
+     */
+    public function passes($attribute, $value)
+    {
+        return COUNTIES[$this->county]::whereId($value)->exists();
+    }
+
+    /**
+     * Get the validation error message.
+     *
+     * @return string
+     */
+    public function message()
+    {
+        return 'Unknown county';
+    }
+}
