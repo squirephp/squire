aaf268f3a6f8312dc0d21d2fced9bba5a9d001f1
Dan Harrin <git@danharrin.com>
2021-10-15 18:42:37 +0100

    Update DataConsistencyTest.php

--- tests/DataConsistencyTest.php
+++ tests/DataConsistencyTest.php
@@ -6,37 +6,44 @@ use Illuminate\Support\Collection;
 
 class DataConsistencyTest extends TestCase
 {
-    protected const PACKAGES_DIRECTORY = "./packages/";
+    protected const PACKAGES_DIRECTORY = './packages/';
 
     /** @test */
     public function are_data_files_consistent(): void
     {
-        $packages = collect(glob(static::PACKAGES_DIRECTORY . "*", GLOB_ONLYDIR))
-            ->filter(fn(string $name) => $this->isTranslatedPackageDirectory($name))
-            ->map(fn(string $name) => str_replace(static::PACKAGES_DIRECTORY, "", $name))
-            ->groupBy(function (string $name) {
-                $parts = explode("-", $name);
+        $packages = collect(glob(static::PACKAGES_DIRECTORY . '*', GLOB_ONLYDIR))
+            ->filter(fn (string $name): bool => $this->isTranslatedPackageDirectory($name))
+            ->map(fn (string $name): string => str_replace(static::PACKAGES_DIRECTORY, '', $name))
+            ->groupBy(function (string $name): string {
+                $parts = explode('-', $name);
                 unset($parts[count($parts) - 1]);
-                return implode("-", $parts);
-            });
-
-        $packages->each(function (Collection $translations, string $package) {
-            if ($translations->count() > 1) {
+                
+                return implode('-', $parts);
+            })
+            ->filter(fn (Collection $translations, string $package): bool => $translations->count() > 1)
+            ->each(function (Collection $translations): void {
                 $previous = null;
+                
                 $translations->each(function (string $translation) use ($package, &$previous) {
-                    $lines = count(explode("\n", file_get_contents(static::PACKAGES_DIRECTORY . $translation . "/resources/data.csv")));
-                    if (!is_null($previous)) {
-                        $this->assertSame($lines, $previous, "Number of data entires in $package is not the same for translations ($translation).");
+                    $lines = count(explode('\n', file_get_contents(static::PACKAGES_DIRECTORY . $translation . '/resources/data.csv')));
+                    
+                    if ($previous !== null) {
+                        $this->assertSame($lines, $previous, "Number of data entires in {$package} is not the same for translations ($translation).");
                     }
+                    
                     $previous = $lines;
                 });
-            }
-        });
+            });
     }
 
     protected function isTranslatedPackageDirectory(string $name): bool
     {
-        $parts = explode("-", $name);
-        return count($parts) > 1 && strlen($parts[count($parts) - 1]) === 2;
+        $parts = explode('-', $name);
+        
+        if (count($parts) <= 1) {
+            return false;
+        }
+            
+        return strlen($parts[count($parts) - 1]) === 2;
     }
 }
